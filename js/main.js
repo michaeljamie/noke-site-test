8ed0e4f149a913cbfe4959622210f8265ae0fa2f
Michael Johnston <mike@noke.com>
2019-01-30 10:41:02 -0700

    push change test

--- js/main.js
+++ js/main.js
@@ -16,21 +16,3 @@ if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
 }
 }
 
-// Hide Address Bar on Scroll
-
-function hideAddressBar()
-{
-  if(!window.location.hash)
-  {
-      if(document.height < window.outerHeight)
-      {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
-      }
-
-      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-  }
-}
-
-window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
-window.addEventListener("orientationchange", hideAddressBar );
-
